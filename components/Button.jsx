5bc4137ce37f3b0a570f1cb2a468118e0eca7103
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2020-12-21 08:06:38 +0100

    Implement modes on pad

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import classNames from "classnames"
 import styles from "./Button.scss"
 
-const Button = ({ active = false, onClick, children }) => {
+const Button = ({ active = false, noPadding = false, onClick, children }) => {
   const [pressed, setPressed] = useState(false)
 
   function onClickInternal(e) {
@@ -20,7 +20,7 @@ const Button = ({ active = false, onClick, children }) => {
     setPressed(false)
   }
 
-  return <div className={classNames("button", { active: active || pressed })}
+  return <div className={classNames("button", { active: active || pressed, noPadding })}
       onClick={onClickInternal} onMouseDown={onMouseDown} onMouseUp={onMouseUp}>
     {children}
     <style jsx>{styles}</style>
--- components/Pad.jsx
+++ components/Pad.jsx
@@ -1,6 +1,7 @@
 import Button from "./Button"
 import { TYPE_DIGITS, ACTION_SET, ACTION_REMOVE } from "./lib/Actions"
 import { MODE_NORMAL, MODE_CORNER, MODE_CENTRE, MODE_COLOUR } from "../components/lib/Modes"
+import classNames from "classnames"
 import styles from "./Pad.scss"
 
 const Pad = ({ updateGame, mode, setMode }) => {
@@ -19,28 +20,68 @@ const Pad = ({ updateGame, mode, setMode }) => {
     })
   }
 
+  const digits = [{
+    digit: 1,
+    corner: "top-left",
+    colour: "#000"
+  }, {
+    digit: 2,
+    corner: "top",
+    colour: "#cfcfcf"
+  }, {
+    digit: 3,
+    corner: "top-right",
+    colour: "#fff"
+  }, {
+    digit: 4,
+    corner: "left",
+    colour: "#a3e048"
+  }, {
+    digit: 5,
+    colour: "#d23be7"
+  }, {
+    digit: 6,
+    corner: "right",
+    colour: "#eb7532"
+  }, {
+    digit: 7,
+    corner: "bottom-left",
+    colour: "#e6261f"
+  }, {
+    digit: 8,
+    corner: "bottom",
+    colour: "#f7d038"
+  }, {
+    digit: 9,
+    corner: "bottom-right",
+    colour: "#34bbe6"
+  }]
+
   return <div className="pad">
-    <div className="left">
+    <div className="pad-left">
       <Button active={mode === MODE_NORMAL} onClick={() => setMode(MODE_NORMAL)}>Normal</Button>
       <Button active={mode === MODE_CORNER} onClick={() => setMode(MODE_CORNER)}>Corner</Button>
       <Button active={mode === MODE_CENTRE} onClick={() => setMode(MODE_CENTRE)}>Centre</Button>
       <Button active={mode === MODE_COLOUR} onClick={() => setMode(MODE_COLOUR)}>Colour</Button>
     </div>
-    <div className="right">
-      <Button active onClick={() => onDigit(1)}>1</Button>
-      <Button active onClick={() => onDigit(2)}>2</Button>
-      <Button active onClick={() => onDigit(3)}>3</Button>
-      <Button active onClick={() => onDigit(4)}>4</Button>
-      <Button active onClick={() => onDigit(5)}>5</Button>
-      <Button active onClick={() => onDigit(6)}>6</Button>
-      <Button active onClick={() => onDigit(7)}>7</Button>
-      <Button active onClick={() => onDigit(8)}>8</Button>
-      <Button active onClick={() => onDigit(9)}>9</Button>
+    <div className="pad-right">
+      {digits.map(d => (
+        <Button key={d.digit} noPadding active onClick={() => onDigit(d.digit)}>
+          <div className="digit-container">
+            <div className={classNames({ centre: mode === MODE_CENTRE,
+                corner: mode === MODE_CORNER, [d.corner]: mode === MODE_CORNER,
+                colour: mode === MODE_COLOUR })}>
+              {mode === MODE_COLOUR || d.digit}
+              {mode === MODE_COLOUR && <div className="colour" style={{ backgroundColor: d.colour }}></div>}
+            </div>
+          </div>
+        </Button>
+      ))}
       <div className="delete">
         <Button onClick={onDelete}>Delete</Button>
       </div>
     </div>
-    <div className="bottom">
+    <div className="pad-bottom">
       <Button>Undo</Button>
       <Button>Redo</Button>
       <Button>Restart</Button>
