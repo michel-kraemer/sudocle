1b2d8871ba4e674e03e36203dfd4e579b4bd0338
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2021-01-09 16:15:43 +0100

    Add favicons

--- pages/[[...id]]/index.jsx
+++ pages/[[...id]]/index.jsx
@@ -286,7 +286,27 @@ const Index = () => {
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
       <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet"/>
       <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@700&display=swap" rel="stylesheet"/>
-      <title>Sudoku</title>
+
+      <link rel="shortcut icon" href={`${process.env.basePath}/favicons/favicon.ico`}/>
+      <link rel="icon" type="image/png" sizes="16x16" href={`${process.env.basePath}/favicons/favicon-16x16.png`}/>
+      <link rel="icon" type="image/png" sizes="32x32" href={`${process.env.basePath}/favicons/favicon-32x32.png`}/>
+      <link rel="icon" type="image/png" sizes="48x48" href={`${process.env.basePath}/favicons/favicon-48x48.png`}/>
+      <link rel="apple-touch-icon" sizes="57x57" href={`${process.env.basePath}/favicons/apple-touch-icon-57x57.png`}/>
+      <link rel="apple-touch-icon" sizes="60x60" href={`${process.env.basePath}/favicons/apple-touch-icon-60x60.png`}/>
+      <link rel="apple-touch-icon" sizes="72x72" href={`${process.env.basePath}/favicons/apple-touch-icon-72x72.png`}/>
+      <link rel="apple-touch-icon" sizes="76x76" href={`${process.env.basePath}/favicons/apple-touch-icon-76x76.png`}/>
+      <link rel="apple-touch-icon" sizes="114x114" href={`${process.env.basePath}/favicons/apple-touch-icon-114x114.png`}/>
+      <link rel="apple-touch-icon" sizes="120x120" href={`${process.env.basePath}/favicons/apple-touch-icon-120x120.png`}/>
+      <link rel="apple-touch-icon" sizes="144x144" href={`${process.env.basePath}/favicons/apple-touch-icon-144x144.png`}/>
+      <link rel="apple-touch-icon" sizes="152x152" href={`${process.env.basePath}/favicons/apple-touch-icon-152x152.png`}/>
+      <link rel="apple-touch-icon" sizes="167x167" href={`${process.env.basePath}/favicons/apple-touch-icon-167x167.png`}/>
+      <link rel="apple-touch-icon" sizes="180x180" href={`${process.env.basePath}/favicons/apple-touch-icon-180x180.png`}/>
+      <link rel="apple-touch-icon" sizes="1024x1024" href={`${process.env.basePath}/favicons/apple-touch-icon-1024x1024.png`}/>
+      <meta name="apple-mobile-web-app-capable" content="yes"/>
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+      <meta name="apple-mobile-web-app-title" content="Sudocle"/>
+
+      <title>Sudocle</title>
     </Head>
     <div className="app" data-theme={settings.theme} data-colour-palette={settings.colourPalette}
         onMouseDown={onMouseDown} ref={appRef}>
