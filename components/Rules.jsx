ce8f8c8ed31b75d043b78fe387a1013b92638a70
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2021-05-15 14:16:38 +0200

    Add missing component files

---
+++ components/Rules.jsx
@@ -0,0 +1,16 @@
+import GameContext from "./contexts/GameContext"
+import { useContext } from "react"
+import styles from "./Rules.scss"
+
+const Rules = () => {
+  const game = useContext(GameContext.State)
+
+  return (<>
+    <h2>{game.data.title}</h2>
+    {game.data.author && <div className="author">by {game.data.author}</div>}
+    <p>{game.data.rules}</p>
+    <style jsx>{styles}</style>
+  </>)
+}
+
+export default Rules
