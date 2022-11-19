c11411750fa8e38bc67d93c072f5c9db82a0b051
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2022-11-19 07:34:15 +0100

    Improve error handling

--- pages/puzzles/[[...id]].tsx
+++
@@ -1,7 +0,0 @@
-// static puzzles should always be served from the `publics` directory
-// return an empty page if the static puzzle file was not found
-const Empty = () => {
-  return <></>
-}
-
-export default Empty
