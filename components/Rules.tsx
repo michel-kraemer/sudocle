10adf58530930ff06b266924156d28845dd695c0
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-11-28 11:42:34 +0100

    Migrate rules page to tailwind

--- components/Rules.tsx
+++ components/Rules.tsx
@@ -1,6 +1,5 @@
 import { State as GameContextState } from "./contexts/GameContext"
 import { useContext } from "react"
-import styles from "./Rules.oscss"
 
 const Rules = () => {
   const game = useContext(GameContextState)
@@ -8,9 +7,8 @@ const Rules = () => {
   return (
     <div className="sidebar-page">
       <h2>{game.data.title}</h2>
-      {game.data.author && <div className="author">by {game.data.author}</div>}
-      <p className="rules">{game.data.rules}</p>
-      <style jsx>{styles}</style>
+      {game.data.author && <div className="lead">by {game.data.author}</div>}
+      <p className="whitespace-pre-wrap">{game.data.rules}</p>
     </div>
   )
 }
